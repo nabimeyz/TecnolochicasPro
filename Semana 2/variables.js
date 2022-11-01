5b1a7d83539f7bfb191222d42a5ed61adfb3c8c5
Metztli Huertero Granada <114110475+nabimeyz@users.noreply.github.com>
2022-10-31 19:13:24 -0600

    Initial commit

---
+++ Semana 2/variables.js
@@ -0,0 +1,17 @@
+let x = 3;
+let y = 4;
+let z;
+z = x + y;
+console.log('La suma de '+x+' y '+y+'es: 'z);
+z = x - y
+console.log(z)
+z = x / y
+console.log(z)
+z = x * y
+console.log(z)
+
+//operadores de comparación
+
+console.log(x==y)
+console.log(x!=y)
+console.log()
